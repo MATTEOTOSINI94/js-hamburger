614a09991bcec6ce51d0a054d52bfd742058cf00
MATTEOTOSINI94 <matteotosini300@icloud.com>
2021-10-11 18:01:21 +0200

    mod

--- main.js
+++ main.js
@@ -3,15 +3,15 @@ const hbMenu = document.querySelector(".hamburger-menu");
 
 console.log(hbMenu.classList);
 
-
-
 btnMenu.addEventListener("click", function(){
-    hbMenu.style.display = "block";
+    // hbMenu.style.display = "block";
+    hbMenu.classList.toggle("active")
 })
 
 const closeMenu = document.querySelector(".close")
 
 closeMenu.addEventListener("click", function(){
-    hbMenu.style.display = "none";
+    // hbMenu.style.display = "none";
+    hbMenu.classList.toggle("active")
 })
 
